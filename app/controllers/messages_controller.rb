d378a9030de57345e74ba0f18eeffde54df1fe43
anko55555 <rosewater0824@yahoo.co.jp>
2016-01-07 12:02:03 +0000

    8.4 sakujo tuika

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,30 +1,57 @@
 class MessagesController < ApplicationController
+before_action :set_message, only: [:edit, :update, :destroy]
+# Messageモデルのオブジェクトの初期化
   def index
-    @message = Message.new
-    # Messageをすべて取得する
-    @messages = Message.all
+  @message = Message.new
+  # Messageをすべて取得する。
+  @messages = Message.all
   end
-  
+
+
+# nameとbodyが含まれたパラメータを受取り、変数@messageに代入。
   def create
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました'
     else
-       #メッセージが保存できなかったとき
-       @messages = Message.all
-       flash.now[:alert] = "メッセージの保存に失敗しました。"
-       render 'index'
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
+   end
+
+# 編集
+  def edit
+  end
+
+# 更新
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
     end
   end
-  
+
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ##　ここまで
+  
+  ## ここまで
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
 end
 
-
-
-  
   
